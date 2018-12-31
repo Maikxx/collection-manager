24b7d685db56452d61f391655a45035f5bb8db97
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-31 14:13:36 +0100

    fix(server): remove file that is no longer used

--- server/db/connect.ts
+++
@@ -1,7 +0,0 @@
-import * as Mongoose from 'mongoose'
-
-export const connectToMongoAtlas = (): void => {
-    const connectionLink = `mongodb://${process.env.MONGO_ATLAS_NAME}:${process.env.MONGO_ATLAS_PW}${process.env.MONGO_ATLAS_CLUSTER}`
-
-    Mongoose.connect(connectionLink, { useNewUrlParser: true })
-}
